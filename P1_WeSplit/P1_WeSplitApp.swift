39a083cb18ecc3cbfbb3f0526516a48d65fed8ef
Manjil Rajbhandari <manjilrajbhandari@gmail.com>
2024-03-26 20:47:03 +0545

    Initial Commit

---
+++ P1_WeSplit/P1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  P1_WeSplitApp.swift
+//  P1_WeSplit
+//
+//  Created by manjil rajbhandari on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct P1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
